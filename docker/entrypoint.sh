2dd942d3d5e7dedb145c504cf732fe9b189dad19
Jan Hruban <jan.hruban@gooddata.com>
2016-05-24 12:16:05 +0200

    Solve PID 1 problem
    'su' has problem reaping child processes. 'bash' should be more suited
    for that. However, using 'bash -c "su user"' still makes 'su' the PID 1,
    for reason unobvious to me. Let's hack a small C program to setuid to
    correct user and run bash as a PID 1.
    
    Change-Id: Id6ce98aa785f4a1a39c72802ed47c8abee9a140f

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,22 @@
 # fallback
 
 USER_ID=${LOCAL_USER_ID:-9001}
+USERSWITCH_SOURCE=/tmp/userswitch.c
+USERSWITCH_BIN=/usr/local/bin/userswitch
+
+cat <<EOF > $USERSWITCH_SOURCE
+#include <unistd.h>
+int main()
+{
+    setuid($USER_ID);
+    execlp( "bash", "bash", 0);
+}
+EOF
+
+gcc -o $USERSWITCH_BIN $USERSWITCH_SOURCE
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
 echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-exec su user
+exec $USERSWITCH_BIN
